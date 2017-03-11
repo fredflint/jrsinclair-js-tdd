59b9c04e740c7901471bcda2306b8be14efba1fb
Yngve Høiseth <yhoiseth@gmail.com>
2017-03-11 21:52:39 +0100

    Add method to create photo list

--- photo-lister-spec.js
+++ photo-lister-spec.js
@@ -7,6 +7,33 @@ describe('PhotoLister', function () {
   })
 });
 
+describe('#photosToHtml()', function () {
+  it('should take an array of photos and turn them into an HTML list', function () {
+    const photos = [
+      {
+        title: 'This is a test',
+        url: 'http://loremflickr.com/960/593'
+      },
+      {
+        title: 'Aaand a second test',
+        url: 'http://loremflickr.com/123/678'
+      }
+    ];
+
+    const expectedHtmlList = [
+      '<ul>',
+      '<li><figure><img src="http://loremflickr.com/960/593" alt=""/>',
+      '<figcaption>This is a test</figcaption></figure></li>',
+      '<li><figure><img src="http://loremflickr.com/123/678" alt=""/>',
+      '<figcaption>Aaand a second test</figcaption></figure></li>',
+      '</ul>',
+    ].join('');
+
+    expect(
+      PhotoLister.photosToHtml(photos)).to.equal(expectedHtmlList);
+  });
+});
+
 describe('#photoToListItem()', function () {
   it('should take a photo object and return a list item string', function () {
     const photos = [
@@ -32,9 +59,7 @@ describe('#photoToListItem()', function () {
     ];
 
     photos.forEach(function (photo, index) {
-      expect(
-        PhotoLister.photoToListItem(photo)).to.equal(expectedListItems[index]
-      );
+      expect(PhotoLister.photoToListItem(photo)).to.equal(expectedListItems[index]);
     });
   });
 });
--- photo-lister.js
+++ photo-lister.js
@@ -8,7 +8,18 @@ const PhotoLister = {
       photo.title,
       '</figcaption></figure></li>',
     ].join('');
-  }
+  },
+  photosToHtml(photos) {
+    const listItems = photos.map(function (photo) {
+      return PhotoLister.photoToListItem(photo);
+    });
+
+    return [
+      '<ul>',
+      listItems.join(''),
+      '</ul>',
+    ].join('');
+  },
 };
 
 module.exports = PhotoLister;
