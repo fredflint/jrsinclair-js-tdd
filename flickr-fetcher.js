3dd87914d6e032d6919aa3907846223cb8814e45
Yngve Høiseth <yhoiseth@gmail.com>
2017-03-11 14:37:06 +0100

    Finish transform photo function

--- flickr-fetcher.js
+++ flickr-fetcher.js
@@ -8,15 +8,15 @@ const FlickrFetcher = {
       metaPhoto.id,
       '_',
       metaPhoto.secret,
-      '_b.jpg'
+      '_b.jpg',
     ];
 
     return urlParts.join('');
   },
   transformMetaPhoto(metaPhoto) {
     return {
-      title: 'Dog goes to desperate measure to avoid walking on a leash',
-      url:   'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg'
+      title: metaPhoto.title,
+      url: this.metaPhotoToUrl(metaPhoto),
     };
   }
 };
